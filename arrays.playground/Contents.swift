3bcdc9a0a5aa534b7207b717bc104462483f1f20
Independent Study <independentstudy@Indesdy.local>
2016-09-16 15:18:11 -0400

    first commit

---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+var names = ["jon", "Jacob", "Jingleheimer", "smith"]
+
+var names2: [String]
+
+names2 = ["qingchongxio"]
+
+var appleproductsiwillown: [String]
+
+appleproductsiwillown = ["macbook", "imac", "ipad", "iphone", "ipod touch", "ipod shuffle", "ipod nano"]
+
+var countriesthatsuck = ["china", "north korea", "honduras"]
+var top3colors = [String](count: 3, repeatedValue: "boring brown")
+
+
